64ebc015681795def1e7e69eaee4c906a1bcb5ed
anuptripathy04 <89001421+AnupTripathy@users.noreply.github.com>
2023-07-08 23:05:13 +0530

    Commit changes

--- Section4/programs/src/ChallengeOne.java
+++ Section4/programs/src/ChallengeOne.java
@@ -1,17 +1,21 @@
 public class ChallengeOne {
     //Screenshot of the problem is available on ..\ScrShots\Challenge
     public static void main(String[] args) {
-        int score1 = 1500;
-        int score2 = 1000;
-        int score3 = 500;
-        int score4 = 100;
-        int score5 = 25;
-
-        System.out.println(calculateHighScore(score1));
-        System.out.println(calculateHighScore(score2));
-        System.out.println(calculateHighScore(score3));
-        System.out.println(calculateHighScore(score4));
-        System.out.println(calculateHighScore(score5));
+        int highScore = calculateHighScore(1500);
+        System.out.println(displayHighScorePosition("Anup", highScore));
+        System.out.println("------------------------------------------------------------------------------------------------------------------");
+        highScore = calculateHighScore(1000);
+        System.out.println(displayHighScorePosition("Adityansu", highScore));
+        System.out.println("------------------------------------------------------------------------------------------------------------------");
+        highScore = calculateHighScore(500);
+        System.out.println(displayHighScorePosition("Amlan", highScore));
+        System.out.println("------------------------------------------------------------------------------------------------------------------");
+        highScore = calculateHighScore(100);
+        System.out.println(displayHighScorePosition("Roronoa", highScore));
+        System.out.println("------------------------------------------------------------------------------------------------------------------");
+        highScore = calculateHighScore(25);
+        System.out.println(displayHighScorePosition("Zenew", highScore));
+        System.out.println("------------------------------------------------------------------------------------------------------------------");
     }
 
     public static String displayHighScorePosition(String pl_name, int pl_pos) {
@@ -21,16 +25,12 @@ public class ChallengeOne {
     public static int calculateHighScore(int score){
         if (score >= 1000) {
             return 1;
-        }
-
-        else if (score >= 500 && score <= 1000) {
+        } else if (score >= 500 && score <= 1000) {
             return 2;
-        }
-
-        else if (score >= 100 && score <= 500) {
+        } else if (score >= 100 && score <= 500) {
             return 3;
+        }else {
+            return 4;
         }
-
-        return 4;
     }
 }
