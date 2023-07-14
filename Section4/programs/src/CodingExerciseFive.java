97bc2572e662fd9eea71c237917c0b5d904872d7
Anup Tripathy <89001421+AnupTripathy@users.noreply.github.com>
2023-07-14 17:50:52 +0530

    Commit Changes

---
+++ Section4/programs/src/CodingExerciseFive.java
@@ -0,0 +1,17 @@
+public class CodingExerciseFive {
+    public static boolean isLeapYear(int year) {
+        if (year >= 1 && year <= 9999) {
+            if (year % 4 == 0) {
+                if (year % 100 == 0) {
+                    if (year % 400 == 0) {
+                        return true;
+                    }
+                }
+                else {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
