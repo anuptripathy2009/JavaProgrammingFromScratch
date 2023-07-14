48c85cb077074ab0ae7db86a01223d3d3721cec5
Anup Tripathy <89001421+AnupTripathy@users.noreply.github.com>
2023-07-14 18:58:07 +0530

    Commit Changes

---
+++ Section4/programs/src/CodingExerciseSeven.java
@@ -0,0 +1,8 @@
+public class CodingExerciseSeven {
+    public static boolean hasEqualSum(int num1, int num2, int sum) {
+        if ((num1 + num2) == sum) {
+            return true;
+        }
+        return false;
+    }
+}
