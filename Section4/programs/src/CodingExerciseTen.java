9b19542601a873e8990f0cadbc0a2eda8996d93e
Anup Tripathy <89001421+AnupTripathy@users.noreply.github.com>
2023-07-15 11:12:06 +0530

    Commit Changes

--- Section4/programs/src/CodingExerciseTen.java
+++ Section4/programs/src/CodingExerciseTen.java
@@ -1,2 +1,19 @@
 public class CodingExerciseTen {
+//    1 hour = 60 minutes
+//    1 day = 24 hour
+//    1 year = 365 days
+    public static void printDaysAndYears(long minute){
+        if (minute < 0) {
+            System.out.println("Invalid Input");
+        }
+        long hour = minute / 60;
+        long days = hour / 24;
+        long years = days / 365;
+        long remaining_days = days % 365;
+        System.out.println(minute + "min = " + years + " y and " + remaining_days + " d");
+    }
+
+    public static void main(String[] args) {
+        printDaysAndYears(525600);
+    }
 }
